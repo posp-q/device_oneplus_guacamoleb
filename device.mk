PeterCxy/android_device_oneplus_guacamole

guacamole: Inherit vendor/oneplus/{}/{}-vendor.mk

device.mk
@@ -6,6 +6,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk
 

posp-q/device_oneplus_guacamoleb

device.mk
@@ -5,6 +5,9 @@
 #
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oneplus/guacamole/guacamoleb-vendor.mk)
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk
 